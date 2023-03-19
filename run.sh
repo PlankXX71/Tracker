0540591041fd5d3868cd37d1a2f72422273504b7
PlankXX71 <116320207+PlankXX71@users.noreply.github.com>
2023-03-19 08:42:09 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,12 +10,11 @@ clear
 banner() {
 printf "\n\e[1;92m"
 printf "\n\e[1;92m████████╗███████╗██████╗ ███████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗"
-printf "\n\e[1;92m"╚══██╔══╝██╔════╝██╔══██╗╚════██║██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝" 
-printf "\n\e[1;92m"   ██║   █████╗  ██████╔╝    ██╔╝██████╔╝███████║██║     █████╔╝ █████"
-printf "\n\e[1;92m"   ██║   ██╔══╝  ██╔══██╗   ██╔╝ ██╔══██╗██╔══██║██║     ██╔═██╗ ╚════██║" 
-printf "\n\e[1;92m"   ██║   ██║     ██║  ██║   ██║  ██║  ██║██║  ██║╚██████╗██║  ██╗███████║"
-printf "\n\e[1;92m"   ╚═╝   ╚═╝     ╚═╝  ╚═╝   ╚═╝  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝"
-                                                                         ▀  \e[1;95mv1.8-BETA\n"
+printf "\n\e[1;92m╚══██╔══╝██╔════╝██╔══██╗╚════██║██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝" 
+printf "\n\e[1;92m   ██║   █████╗  ██████╔╝    ██╔╝██████╔╝███████║██║     █████╔╝ █████"
+printf "\n\e[1;92m   ██║   ██╔══╝  ██╔══██╗   ██╔╝ ██╔══██╗██╔══██║██║     ██╔═██╗ ╚════██║" 
+printf "\n\e[1;92m   ██║   ██║     ██║  ██║   ██║  ██║  ██║██║  ██║╚██████╗██║  ██╗███████║"
+printf "\n\e[1;92m   ╚═╝   ╚═╝     ╚═╝  ╚═╝   ╚═╝  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝" \e[1;95mv1.8-BETA\n"
 printf "\n\e[1;92m   Coded by \e[1;94m@Caviar1989\n\n\e[1;92m"
 }
 dependencies() {
@@ -30,16 +29,7 @@ if [[ $option == 1 || $option == 01 ]]; then
 	printf "\n\nEnter your phone number in international format ...\n"
 	read -p $'\n\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m]\e[1;93m Enter Number: \e[1;92m' optiona
 
-	printf "\n\e[1;92m printf Registrant Contact Information:
-Name Inod sanjaya
-Organization Geekz Inc
-Address JlPemuda Kranji RT02 RW03
-City Bekasi
-State / ProvinceJawa Barat
-Postal Code17135
-Country ID
-Phone +62.85649455626
-Email inodsanjaya1998@gmail.com\n\n\e[1;92m"
+	printf "\n\e[1;92m printf waiting for information....\n\n\e[1;92m"
 	sleep 2.9
 	python3 files/phoneinfoga.py -n $optiona
 	printf "\n\n\e[1;92mNOTE: If you get any problem while using this tool than please report to\nTelegram Group: \e[1;96mhttp://t.me/linux_repo\e[1;92m\n"
